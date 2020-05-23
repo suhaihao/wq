9b098b81656859e19cd6f01b13af310abfaae16b
suhai <505604843@qq.com>
2020-05-23 20:59:32 +0800

    初提交

---
+++ src/main/java/com/bhst/wq/WqApplication.java
@@ -0,0 +1,13 @@
+package com.bhst.wq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WqApplication.class, args);
+    }
+
+}
