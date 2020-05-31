68fc70b4dc8d8496afae4932d6456dd760e1e88a
suhai <505604843@qq.com>
2020-05-31 10:33:34 +0800

    初提交

--- src/main/java/com/bhst/wq/config/security/SecurityConfig.java
+++ src/main/java/com/bhst/wq/config/security/SecurityConfig.java
@@ -6,7 +6,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -45,10 +48,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(selfAuthenticationProvider).eraseCredentials(true);
     }
 
+    private CorsConfigurationSource CorsConfigurationSource() {
+        CorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");    //同源配置，*表示任何请求都视为同源，若需指定ip和端口可以改为如“localhost：8080”，多个以“，”分隔；
+        corsConfiguration.addAllowedHeader("*");//header，允许哪些header，本案中使用的是token，此处可将*替换为token；
+        corsConfiguration.addAllowedMethod("*");    //允许的请求方法，PSOT、GET等
+        ((UrlBasedCorsConfigurationSource) source).registerCorsConfiguration("/**", corsConfiguration); //配置允许跨域访问的url
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable().cors().and()
+        http.csrf().disable().cors().configurationSource(CorsConfigurationSource()).and()
 
                 .httpBasic().authenticationEntryPoint(ajaxAuthenticationEntryPoint)
 
