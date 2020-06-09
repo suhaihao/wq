40a9f904101d855e65e0c56018b47223757c4f25
suhai <505604843@qq.com>
2020-06-10 00:45:36 +0800

    初提交

---
+++ src/main/java/com/bhst/wq/controller/WqUploadController.java
@@ -0,0 +1,73 @@
+package com.bhst.wq.controller;
+
+import com.bhst.wq.exception.BusinessInterfaceException;
+import com.bhst.wq.response.ResultBean;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/upload")
+@CrossOrigin
+@Api("上传接口")
+public class WqUploadController {
+
+
+    @PostMapping("/file")
+    @ApiOperation(value = "上传文件")
+    public ResultBean<String> upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IllegalStateException, IOException {
+        log.info("开始上传");
+
+        boolean isEmpty = file.isEmpty();
+        if (isEmpty) {
+            throw new BusinessInterfaceException("上传失败！上传的文件为空！");
+        }
+
+        long fileSize = file.getSize();
+        log.info("文件大小:{}", fileSize);
+        if (fileSize > 1 * 1024 * 1024) {
+            throw new BusinessInterfaceException("上传失败！上传的文件大小超出了限制！");
+        }
+
+        String contentType = file.getContentType();
+        log.info("文件类型:{}", contentType);
+        List<String> types = new ArrayList<>();
+        types.add("image/jpeg");
+        types.add("image/png");
+        types.add("image/gif");
+        if (!types.contains(contentType)) {
+            throw new BusinessInterfaceException("上传失败！不允许上传此类型的文件！");
+        }
+        String parentDir = request.getServletContext().getRealPath("upload");
+        log.info("文件路径为:{}", parentDir);
+        File parent = new File(parentDir);
+        if (!parent.exists()) {
+            parent.mkdirs();
+        }
+        String originalFilename = file.getOriginalFilename();
+        String filename = UUID.randomUUID().toString();
+        log.info("原文件名:{}修改为:{}", originalFilename, filename);
+        String suffix = "";
+        int beginIndex = originalFilename.lastIndexOf(".");
+        if (beginIndex != -1) {
+            suffix = originalFilename.substring(beginIndex);
+        }
+
+        // 执行保存文件
+        File dest = new File(parent, filename + suffix);
+        file.transferTo(dest);
+        log.info("上传结束");
+        return new ResultBean<>(filename);
+    }
+
+}
